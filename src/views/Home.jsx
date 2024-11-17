2b6726d0b700378ba6b02439e4c2efdf4fe26b01
Teresa <mtmaisterra@gmail.com>
2024-11-16 21:02:07 -0300

    NavBar terminado

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const home = () => {
+const Home = () => {
   return (
     <div>
-     hola mundo
+     
     </div>
   )
 }
 
-export default home;
+export default Home;
---
+++ src/views/QueHacer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const QueHacer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default QueHacer
