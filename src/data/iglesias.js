5547cd3f893934576d1a7d458a5bdca03fdecc03
Teresa <mtmaisterra@gmail.com>
2024-11-18 18:10:20 -0300

    iglesias terminado

---
+++ src/data/iglesias.js
@@ -0,0 +1,41 @@
+import iglesia1 from '../assets/images/IglesiaIsidroLabrador.jpg';
+import iglesia2 from '../assets/images/IglesiaLaMerced.jpg';
+import iglesia3 from '../assets/images/IglesiaSanAntonio.jpg';
+import iglesia4 from '../assets/images/IglesiaSanJose.jpg';
+import iglesia5 from '../assets/images/IglesiaSanPedro.jpg';
+import iglesia6 from '../assets/images/IglesiaSanNicolas.jpg';
+
+const datoIglesias = [
+    {
+        id: 1,
+        photo: iglesia1,
+        description: 'San Isidro Labrador - Agua Blanca',
+    },
+    {
+        id: 2,
+        photo: iglesia2,
+        description: 'Virgen de La Merced - Aminga',
+    },
+    {
+        id: 3,
+        photo: iglesia3,
+        description: 'San Antonio - Anillaco',
+    },
+    {
+        id: 4,
+        photo: iglesia4,
+        description: 'San José - Los Molinos',
+    },
+    {
+        id: 5,
+        photo: iglesia5,
+        description: 'San Pedro - San Pedro',
+    },
+    {
+        id: 6,
+        photo: iglesia6,
+        description: 'San Miguel Arcángel - Pinchas',
+    },
+];
+
+export default datoIglesias;
--- src/views/Informacion.jsx
+++ src/views/Informacion.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Informacion = () => {
   return (
-    <div>
+    <div className='flex items-center'>
       
     </div>
   )
